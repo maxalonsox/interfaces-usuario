ea55bdb079f48a96cf098a566096b2d37ae3006b
Federico Alexander <fedealexander2002@gmail.com>
2023-10-04 21:59:05 -0300

    finalized footer and advanced game-play page

---
+++ TP2/entrega-final/js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+/*
+document.querySelector("#buscar").addEventListener("click", borrar);
+let juegos = [10];
+
+function borrar(){
+    
+    let carrusel = document.querySelector("#carrusel");
+    carrusel.innerHTML = "";
+    for (let index = 0; index < juegos; index++) {
+        carrusel.innerHTML += `
+                                <div class="box-carrusel">
+                                sdasdassad
+                                </div>`
+    }
+
+}*/
