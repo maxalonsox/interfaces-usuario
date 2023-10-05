2fda8fa1f173f63046ec2437724dc853ee4b181d
Federico Alexander <fedealexander2002@gmail.com>
2023-10-05 02:49:05 -0300

    Compartir en redes listo

--- TP2/entrega-final/js/main.js
+++ TP2/entrega-final/js/main.js
@@ -1,17 +1,10 @@
 'use strict';
-/*
-document.querySelector("#buscar").addEventListener("click", borrar);
-let juegos = [10];
 
-function borrar(){
-    
-    let carrusel = document.querySelector("#carrusel");
-    carrusel.innerHTML = "";
-    for (let index = 0; index < juegos; index++) {
-        carrusel.innerHTML += `
-                                <div class="box-carrusel">
-                                sdasdassad
-                                </div>`
-    }
+let popUpRedes = document.querySelector("#popUpRedes");
+document.querySelector("#btn-compartir").addEventListener("click", ()=>{
+    popUpRedes.showModal();
+});
 
-}*/
+document.querySelector("#cerrar-popUpRedes").addEventListener("click", ()=>{
+    popUpRedes.close();
+});
