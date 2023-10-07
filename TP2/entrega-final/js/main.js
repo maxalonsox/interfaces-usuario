5891c8db9f27a936ebc7b3c95ff8eb399c77349b
Federico Alexander <fedealexander2002@gmail.com>
2023-10-07 01:05:08 -0300

    Se empezo popUp del user

--- TP2/entrega-final/js/main.js
+++ TP2/entrega-final/js/main.js
@@ -1,6 +1,19 @@
 'use strict';
 
+//POPUP USER
+let popUpUser = document.querySelector("#popUpUser");
+
+document.querySelector("#btn-user").addEventListener("click", ()=>{
+    popUpUser.showModal();
+});
+
+document.querySelector("#cerrar-popUpUser").addEventListener("click", ()=>{
+    popUpUser.closeModal();
+});
+
+//POPUP REDES
 let popUpRedes = document.querySelector("#popUpRedes");
+
 document.querySelector("#btn-compartir").addEventListener("click", ()=>{
     popUpRedes.showModal();
 });
